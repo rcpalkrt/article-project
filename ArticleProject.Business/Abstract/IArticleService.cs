1d39e78cb633e7a44d5cbec085be511183882933
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 15:41:17 +0300

    article service created

---
+++ ArticleProject.Business/Abstract/IArticleService.cs
@@ -0,0 +1,19 @@
+﻿using ArticleProject.Entities.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleProject.Business.Abstract
+{
+    public interface IArticleService
+    {
+        Task<bool> SaveAsync(ArticleForSaveDto model);
+
+        Task<List<ArticleForListDto>> GetList();
+
+        Task<List<ArticleForListDto>> FindList();
+
+        Task<bool> Delete(int id);
+    }
+}
