1865bde52f3e31f864d78a915229f693ef91c467
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 16:16:27 +0300

    mapping profile created

---
+++ ArticleProject.CrossCuttingConcerns/AutoMapper/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using ArticleProject.Entities.Concrete;
+using ArticleProject.Entities.DataTransferObject;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleProject.CrossCuttingConcerns.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Article, ArticleForSaveDto>()
+                .ForMember(a => a.ReleaseDate, dest => dest.MapFrom(src => src.ReleaseDate.ToString("dd MMMM yyyy")));
+
+            CreateMap<ArticleForSaveDto, Article>()
+                .ForMember(a => a.ReleaseDate, dest => dest.MapFrom(src => DateTime.Parse(src.ReleaseDate)));
+
+            CreateMap<Article, ArticleForListDto>()
+                .ForMember(a => a.ReleaseDate, dest => dest.MapFrom(src => src.ReleaseDate.ToString("dd MMMM yyyy")));
+        }
+    }
+}
