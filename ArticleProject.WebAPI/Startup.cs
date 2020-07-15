724df3daa318ed6226aa916bf781927308dde078
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 17:04:17 +0300

    update web api startup

--- ArticleProject.WebAPI/Startup.cs
+++ ArticleProject.WebAPI/Startup.cs
@@ -2,9 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArticleProject.Business.Abstract;
+using ArticleProject.Business.Concrete;
+using ArticleProject.CrossCuttingConcerns.AutoMapper;
+using ArticleProject.DataAccess.Abstract;
+using ArticleProject.DataAccess.Concrete;
+using ArticleProject.DataAccess.Contexts;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +32,23 @@ namespace ArticleProject.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContext<SqlDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUnitOfWorkRepository, EfUnitOfWorkRepository>();
+            services.AddScoped<IArticleRepository, EfArticleRepository>();
+            services.AddScoped<IArticleService, ArticleManager>();
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +60,13 @@ namespace ArticleProject.WebAPI
             }
 
             app.UseRouting();
+
+            app.UseCors(builder => builder
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .SetIsOriginAllowed((host) => true)
+              .AllowCredentials()
+              );
 
             app.UseAuthorization();
 
