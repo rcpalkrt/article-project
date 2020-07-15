b4e30070d3ffd8da5a08289063e65dfd1d1e6cca
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 13:19:08 +0300

    unit of work created

---
+++ ArticleProject.Core/DataAccess/Abstract/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleProject.Core.DataAccess.Abstract
+{
+    public interface IUnitOfWork
+    {
+        Task CompleteAsync();
+    }
+}
---
+++ ArticleProject.Core/DataAccess/Concrete/EfUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using ArticleProject.Core.DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticleProject.Core.DataAccess.Concrete
+{
+    public class EfUnitOfWork : IUnitOfWork
+    {
+        private DbContext _dbContext;
+
+        public EfUnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CompleteAsync()
+        {
+            using (var transaction = _dbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    await _dbContext.SaveChangesAsync();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw new Exception(ex.Message);
+                }
+
+                finally
+                {
+                    transaction.Dispose();
+                }
+            }
+
+        }
+    }
+}
