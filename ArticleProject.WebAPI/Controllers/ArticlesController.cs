8c6cf792a9055617ece82a1c838c7baae6a09268
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 17:09:06 +0300

    article controller created

---
+++ ArticleProject.WebAPI/Controllers/ArticlesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArticleProject.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArticleProject.WebAPI.Controllers
+{
+    [Route("api/articles")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private IArticleService _articleService;
+
+        public ArticlesController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+    }
+}
