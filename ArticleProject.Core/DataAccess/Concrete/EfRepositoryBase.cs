b008741cb4e851296908aa21155a841ee25a9d94
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 13:13:51 +0300

    update repository base

--- ArticleProject.Core/DataAccess/Concrete/EfRepositoryBase.cs
+++ ArticleProject.Core/DataAccess/Concrete/EfRepositoryBase.cs
@@ -1,7 +1,9 @@
 ﻿using ArticleProject.Core.DataAccess.Abstract;
 using ArticleProject.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,39 +12,54 @@ namespace ArticleProject.Core.DataAccess.Concrete
 {
     public class EfRepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class, IEntity, new()
     {
+        private DbContext _dbContext;
+
+        public EfRepositoryBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            var entity = await _dbContext.Set<TEntity>().SingleOrDefaultAsync(filter);
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> FindListAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            var entities = await _dbContext.Set<TEntity>().Where(filter).ToListAsync();
+
+            return entities;
+        }
+
+        public async Task<TEntity> GetAsync(int id)
+        {
+            var entity = await _dbContext.Set<TEntity>().FindAsync(id);
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync()
+        {
+            var entities = await _dbContext.Set<TEntity>().ToListAsync();
+
+            return entities;
+        }
+
         public void Add(TEntity entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Add(entity);
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<TEntity>> FindListAsync(Expression<Func<TEntity, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> GetAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<TEntity>> GetListAsync()
-        {
-            throw new NotImplementedException();
+            _dbContext.Remove(entity);
         }
 
         public void Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Set<TEntity>().Update(entity);
         }
     }
 }
