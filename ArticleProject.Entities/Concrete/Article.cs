55d67dabb68091babb9a593e202f1b85ca5e1aaa
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 12:49:32 +0300

    article entity and base entity created

---
+++ ArticleProject.Entities/Concrete/Article.cs
@@ -0,0 +1,16 @@
+﻿using ArticleProject.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleProject.Entities.Concrete
+{
+    public class Article : BaseEntity, IEntity
+    {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Abstract { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
