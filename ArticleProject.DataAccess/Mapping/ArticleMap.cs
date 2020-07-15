e5e817e9867c7981aad92ce70bb09115ca6d20d2
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 13:01:18 +0300

    article map created

---
+++ ArticleProject.DataAccess/Mapping/ArticleMap.cs
@@ -0,0 +1,33 @@
+﻿using ArticleProject.Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleProject.DataAccess.Mapping
+{
+    public class ArticleMap
+    {
+        public ArticleMap(EntityTypeBuilder<Article> entity)
+        {
+            entity.HasKey(a => a.ID);
+
+            entity.Property(a => a.Title)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            entity.Property(a => a.Author)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            entity.Property(a => a.Abstract)
+                .IsRequired();
+
+            entity.Property(a => a.Summary)
+                .IsRequired();
+
+            entity.Property(a => a.Content)
+                .IsRequired();
+        }
+    }
+}
