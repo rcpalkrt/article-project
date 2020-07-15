bcace19d62c27203ee59920a60f7619373d18cd5
recepalkurt <alkurtrecep@gmail.com>
2020-07-15 12:21:55 +0300

    sql context class created

---
+++ ArticleProject.DataAccess/Contexts/SqlDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticleProject.DataAccess.Contexts
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
